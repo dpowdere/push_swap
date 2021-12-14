bbba370da46db8869b2f07370224b2cb09ad9361
Dorine Powdered <dpowdere@student.21-school.ru>
2021-12-14 20:15:50 +0300

    temp

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 17:55:08 by dpowdere          #+#    #+#             */
-/*   Updated: 2021/12/09 17:57:01 by dpowdere         ###   ########.fr       */
+/*   Updated: 2021/12/14 20:15:34 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,9 @@ int	main(int argc, char *argv[])
 {
 	(void)argc;
 	(void)argv;
+	// - split args into array of integers
+	// - check for duplicates with self-balancing trees?
+	// - create stacks
+	// - populate stack a
 	return (EXIT_SUCCESS);
 }
