161036b690baecd4fe9675577e51ec6dd6a5d9d4
Dorine Powdered <dpowdere@student.21-school.ru>
2022-01-17 22:24:01 +0300

    Test swapping stack

--- src/push_swap.c
+++ src/push_swap.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   push_top_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 17:55:08 by dpowdere          #+#    #+#             */
-/*   Updated: 2022/01/17 22:07:58 by dpowdere         ###   ########.fr       */
+/*   Updated: 2022/01/17 22:22:41 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,37 @@ int	main(int argc, char *argv[])
 	ft_stack_push(stack, new_int(33));
 	ft_stack_push(stack, new_int(-345));
 	print_stack(stack);
+
+	ft_stack_top_swap(stack);
+	print_stack(stack);
+
+	int	*p;
+
+	p = ft_stack_pop(stack);
+	free(p);
+	print_stack(stack);
+
+	ft_stack_top_swap(stack);
+	print_stack(stack);
+
+	p = ft_stack_pop(stack);
+	free(p);
+	print_stack(stack);
+
+	ft_stack_top_swap(stack);
+	print_stack(stack);
+
+	p = ft_stack_pop(stack);
+	free(p);
+	print_stack(stack);
+
+	ft_stack_top_swap(stack);
+	print_stack(stack);
+
+	p = ft_stack_pop(stack);
+	free(p);
+	print_stack(stack);
+
 	ft_stack_destroy(&stack, free);
 	return (EXIT_SUCCESS);
 }
