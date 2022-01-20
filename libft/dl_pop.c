2e23c09b359b4f6aa691a5da78c3f72bdc01aed6
Dorine Powdered <dpowdere@student.21-school.ru>
2022-01-20 17:16:13 +0300

    Refactor to comply with the norme

--- libft/dl_pop.c
+++ libft/dl_pop.c
@@ -6,7 +6,7 @@
 /*   By: dpowdere <dpowdere@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 08:36:02 by dpowdere          #+#    #+#             */
-/*   Updated: 2022/01/20 17:03:47 by dpowdere         ###   ########.fr       */
+/*   Updated: 2022/01/20 17:14:43 by dpowdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,9 @@
 ** Pop an element. When a pointer to a list is its head or tail, update
 ** the pointer accordingly. Do nothing, if it points to an intermediate link.
 */
-void	*dl_pop(t_dlist **lst)
-{
-	void	*content;
-	t_dlist	*elem;
 
-	if (!lst || !*lst)
-		return (NULL);
-	elem = *lst;
-	content = elem->content;
+static inline void	dl__relink(t_dlist **lst, t_dlist *elem)
+{
 	if (elem->next && elem->prev)
 	{
 		elem->next->prev = elem->prev;
@@ -44,5 +38,18 @@ void	*dl_pop(t_dlist **lst)
 	}
 	else
 		*lst = NULL;
+}
+
+void	*dl_pop(t_dlist **lst)
+{
+	void	*content;
+	t_dlist	*elem;
+
+	if (!lst || !*lst)
+		return (NULL);
+	elem = *lst;
+	content = elem->content;
+	dl__relink(lst, elem);
+	free(elem);
 	return (content);
 }
